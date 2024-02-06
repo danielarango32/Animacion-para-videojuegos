e279c468a45a5122de9b67cd723a44fa87dab687
danielarango32 <77700657+danielarango32@users.noreply.github.com>
2024-02-05 19:59:56 -0500

    clase

--- Assets/Scripts/animationController.cs
+++ Assets/Scripts/animationController.cs
@@ -6,14 +6,20 @@ using CallbackContext = UnityEngine.InputSystem.InputAction.CallbackContext;
 
 public class animationController : MonoBehaviour
 {
-    Animator animator;
-    Vector2 currentInput;
-    Vector2 nextInput;
-    Vector2 inputVelocity;
-
-
+    private Animator animator;
+    private Vector2 currentInput;
+    private Vector2 nextInput;
+    private Vector2 inputVelocity;
+    private Motionstates motionstates = Motionstates.NotInCombat;
+    private Vector3 proyectedVector;
 
     int MotionXId, MotionYId;
+
+    enum Motionstates
+    {
+        NotInCombat,
+        InCombat
+    }
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -30,7 +36,25 @@ public class animationController : MonoBehaviour
     public void Update()
     {
         currentInput = Vector2.SmoothDamp(currentInput, nextInput,ref inputVelocity, 0.4f);
-        animator.SetFloat(MotionXId, currentInput.x);
-        animator.SetFloat(MotionYId, currentInput.y);
+
+        if (motionstates == Motionstates.NotInCombat)
+        {
+            //Calcular direccin de movimientos
+            Transform cameraTransform =Camera.main.transform;
+            Vector3 cameraFoward = Vector3.Lerp(cameraTransform.forward, cameraTransform. up, Mathf.Abs(Vector3.Dot(cameraTransform.forward, transform.up)));
+            proyectedVector = Vector3.ProjectOnPlane(cameraFoward, transform.up);
+
+            //Rotar la direccion de movimiento
+            //Setear la animación
+            animator.SetFloat(MotionYId, currentInput.y);
+        }
+    
+        
     }
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + proyectedVector);
+    }
+#endif
 }
