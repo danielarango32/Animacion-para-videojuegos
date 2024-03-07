bd87fed78f0de753b916b44b7185ef211a21450e
danielarango32 <77700657+danielarango32@users.noreply.github.com>
2024-03-06 22:18:40 -0500

    111

--- Assets/Scenes/rigify/FootIK.cs
+++ Assets/Scenes/rigify/FootIK.cs
@@ -3,15 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]  
 public class FootIK : MonoBehaviour
 {
     [SerializeField] private Transform detectionReference;
     [SerializeField] private Transform foot;
     [SerializeField][Range(0,1)] private float detectionRange;
     [SerializeField] private float maxDetectionDistance;
+    [SerializeField] private AvatarIKGoal ikGoal;
+    [SerializeField] private Vector2 snapOffsets;
+    [SerializeField] private string snapOffsetParameter;
+    [SerializeField] private float snapSpeed = 15;
+
+    Animator animator;
 
     private bool hasTarget;
     private RaycastHit ikTarget;
+    private Vector3 currentIkPosition;
     // Start is called before the first frame update
 
     public Vector3 GetDetectionStartPosition()
@@ -21,14 +29,23 @@ public class FootIK : MonoBehaviour
         return detectionReference.TransformPoint(ret);
     }
 
-    private void GetTargetPosition()
+    private bool GetTargetPosition()
     {
-        hasTarget = Physics.Raycast(GetDetectionStartPosition(), -detectionReference.up, out ikTarget, maxDetectionDistance);
+        return Physics.Raycast(GetDetectionStartPosition(), -detectionReference.up, out ikTarget, maxDetectionDistance);
     }
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
     private void OnAnimatorIK(int layerIndex)
     {
-        GetTargetPosition();
+        hasTarget = GetTargetPosition();
+        currentIkPosition = Vector3.Lerp(currentIkPosition, ikTarget.point, Time.deltaTime * snapSpeed);
+        animator.SetIKPositionWeight(ikGoal, 1.0f);
+        float snapInterpolator = animator.GetFloat(snapOffsetParameter);
+        float solvedSnapOffset = Mathf.Lerp(snapOffsets.x, snapOffsets.y, snapInterpolator);    
+        animator.SetIKPosition(ikGoal, currentIkPosition + detectionReference.up * snapOffsets.y);
     }
 
     public Transform DetectionReference => detectionReference;
