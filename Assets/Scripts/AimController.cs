f55427f05ee846a80d879beae55802fc1934fa47
Juan Sebastian Cadavid Carcamo <juansecadavid@gmail.com>
2024-04-06 23:25:45 -0500

    aimUpdated

--- Assets/Scripts/AimController.cs
+++ Assets/Scripts/AimController.cs
@@ -26,23 +26,27 @@ public class AimController : ThirdPersonShooterPlayerScript
         if (cameraRotationTarget == null)
             return;
         cameraRotationTarget.RotateAround(transform.position, transform.up, inputValue.x * Time.deltaTime * 1.3f);
-        
-
     }
 
     public void Aiming(InputAction.CallbackContext context)
     {
         Vector2 inputValue = context.ReadValue<Vector2>();
-        Vector3 projectedPoint = camera.ScreenToWorldPoint(new Vector3(inputValue.x, inputValue.y, 10));
-        Debug.DrawLine(camera.transform.position, projectedPoint, Color.cyan, 0.3f);
-        aimTarget.position = projectedPoint;
         Vector2 normalizedPointerPosition = inputValue / new Vector2(camera.pixelWidth, camera.pixelHeight);
         bool shouldRotateHorizontal = normalizedPointerPosition.x < horizontalThreshold.x ||
                                       normalizedPointerPosition.x > horizontalThreshold.y;
         bool shouldRotateVertical = normalizedPointerPosition.y < verticalThreshold.x ||
-                                      normalizedPointerPosition.y > verticalThreshold.y;
+                                    normalizedPointerPosition.y > verticalThreshold.y;
         Vector2 direcctionalPointerPosition = normalizedPointerPosition * Vector2.one - Vector2.one;
-        aimCameraRig.RotateAround(transform.position, transform.up, direcctionalPointerPosition.x * 1.3f);
+        if (shouldRotateHorizontal)
+        {
+            aimCameraRig.RotateAround(transform.position, transform.up, direcctionalPointerPosition.x * lookSpeed * Time.deltaTime * 1.3f);
+        }
+        Vector3 projectedPoint = camera.ScreenToWorldPoint(new Vector3(inputValue.x, inputValue.y, 10));
+        Debug.DrawLine(camera.transform.position, projectedPoint, Color.cyan, 0.3f);
+        aimTarget.position = projectedPoint;
+        
+        
+        
 
     }
     public void ToogleAim(InputAction.CallbackContext context)
